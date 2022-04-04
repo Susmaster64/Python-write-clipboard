17339e160026b7fbc45b175bba0ca8774d9d782f
Susmaster64 <99788724+Susmaster64@users.noreply.github.com>
2022-04-04 08:46:08 +0000

    YES

---
+++ sus.py
@@ -0,0 +1,63 @@
+import time
+import random
+import win32api, win32con, win32clipboard
+from pynput.keyboard import Key, Controller
+import keyboard as kboard
+
+keyboard = Controller()
+
+print('Started! hold the \'ctrl\' key to run script.')
+
+while kboard.is_pressed('ctrl') == False:
+    
+    time.sleep(0.5)
+    print('Started! hold the \'ctrl\' key to run script.')
+    
+time.sleep(0.5)
+print('Starting!')
+time.sleep(0.1)
+
+while kboard.is_pressed('Esc') == False:
+
+    print('Thinking...')
+    time.sleep(random.uniform(1.6, 2.5))
+    print('Finished thunk!')
+    print('Begining processing, hold \'Esc\' to abort.')
+    time.sleep(0.01)
+    
+    while 1:
+        
+        try:
+            
+            print('Trying to access the clipboard.')
+            win32clipboard.OpenClipboard()
+            break
+
+        except Exception:
+        
+            print('Clipboard access failed :( , trying again in 0.5 seconds.')
+            time.sleep(0.5)
+            continue
+        
+    time.sleep(0.01)
+    print('Reading clipboard.')
+    data = win32clipboard.GetClipboardData()
+    win32clipboard.CloseClipboard()
+    time.sleep(0.01)
+    print('Typing...')
+    
+    for n in data:
+        
+        keyboard.type(n)
+        time.sleep(random.uniform(0.01, 0.1))
+        
+        if kboard.is_pressed('Esc'):
+            
+            print('manually aborted.')
+            aborted = True
+            break
+        
+    time.sleep(0.5)
+    print('Finished typing or manually aborted. The answer was \'{}\'.'.format(data))
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
